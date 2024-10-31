e9c9f5f132fefa801e62ae7e84f95bfd0e467201
charrbit <40149235+charrbit@users.noreply.github.com>
2024-10-30 22:45:34 -0500

    feat: add single "pointer" node
    Implements a node with a single pointer to support one-way traversing linked lists

---
+++ singly_node.py
@@ -0,0 +1,9 @@
+from base_node import BaseNode
+
+# singly linked list node with one
+# "pointer" to the next node in the list
+
+class SinglyNode(BaseNode):
+    def __init__(self, data = None):
+        super().__init__(data)
+        self.next = None
