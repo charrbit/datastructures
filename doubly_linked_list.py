8dec811ae2a451d6851556e5e06cbb55f01cbcb7
charrbit <40149235+charrbit@users.noreply.github.com>
2024-11-02 16:42:19 -0500

    feat: doubly linked list node insertion by index
    Overriding base insertion by index to account for a node prev pointer

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -12,4 +12,16 @@ class DoublyLinkedList(SinglyLinkedList):
     def __init__(self):
         super().__init__()
 
+    def insertAtIndex(self, node, index):
+        node, nodeBefore = super().insertAtIndex(node, index)
+        if node != None:
+            if node.next != None:
+                node.next.prev = node
+            node.prev = nodeBefore
+        return node
+
+    def insertAtHead(self, node):
+        return self.insertAtIndex(node, 0)
     
+    def insertAtTail(self, node):
+        return self.insertAtIndex(node, self.length)
