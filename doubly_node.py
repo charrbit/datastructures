ecd035bcf57f8c2fac56423ddb001241be856d70
charrbit <40149235+charrbit@users.noreply.github.com>
2024-11-02 10:26:02 -0500

    feat: add double "pointer" node
    Defines a node with two pointers to support two-way traversing linked lists

---
+++ doubly_node.py
@@ -0,0 +1,9 @@
+from singly_node import SinglyNode
+
+# doubly linked list node with two "pointers" 
+# to the next and previous nodes in the list
+
+class DoublyNode(SinglyNode):
+    def __init__(self, data = None):
+        super().__init__(data)
+        self.prev = None
