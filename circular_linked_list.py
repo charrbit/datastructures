8454158547b14f02267b8761068e237fb78283bc
charrbit <40149235+charrbit@users.noreply.github.com>
2024-11-02 22:42:28 -0500

    feat: circular linked list node removal at index
    Overriding doubly linked list removal by index to account for head and tail nodes pointing to each other

--- circular_linked_list.py
+++ circular_linked_list.py
@@ -28,3 +28,14 @@ class CircularLinkedList(DoublyLinkedList):
     def insertAtTail(self, node):
         return self.insertAtIndex(node, self.length)
     
+    def removeAtIndex(self, index):
+        node = super().removeAtIndex(index)
+        if node != None:
+            if node.prev != None:
+                node.prev.next = node.next
+
+    def removeAtHead(self):
+        return self.removeAtIndex(0)
+
+    def removeAtTail(self):
+        return self.removeAtIndex(self.length - 1)
