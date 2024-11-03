6feb00d400e607562aed1d923abe5098b7a5602d
charrbit <40149235+charrbit@users.noreply.github.com>
2024-11-02 22:22:55 -0500

    feat: circular linked list node insertion at index
    Overriding doubly linked list insertion by index to account for head and tail nodes pointing to each other

--- circular_linked_list.py
+++ circular_linked_list.py
@@ -12,4 +12,19 @@ class CircularLinkedList(DoublyLinkedList):
     def __init__(self):
         super().__init__()
 
+    def insertAtIndex(self, node, index):
+        node = super().insertAtIndex(node, index)
+        if node != None:
+            if node.next == None:
+                node.next = node.prev = node
+            elif node.prev == None:
+                node.prev = self.tail
+                self.tail.next = node
+        return node
+    
+    def insertAtHead(self, node):
+        return self.insertAtIndex(node, 0)
+    
+    def insertAtTail(self, node):
+        return self.insertAtIndex(node, self.length)
     
